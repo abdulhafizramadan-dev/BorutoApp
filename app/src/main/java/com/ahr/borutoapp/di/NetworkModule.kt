3d92ec6758de75f34b0ad2f4cf89ea5e303aad95
abdulhafizramadan-dev <abdulhafizramadan.dev@gmail.com>
2023-03-08 22:02:33 +0700

    | Create NetworkModule

---
+++ app/src/main/java/com/ahr/borutoapp/di/NetworkModule.kt
@@ -0,0 +1,33 @@
+package com.ahr.borutoapp.di
+
+import com.ahr.borutoapp.data.remote.BorutoService
+import com.ahr.borutoapp.util.Constant.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideBorutoService(
+        retrofit: Retrofit
+    ): BorutoService {
+        return retrofit.create(BorutoService::class.java)
+    }
+}
--- app/src/main/java/com/ahr/borutoapp/util/Constant.kt
+++ app/src/main/java/com/ahr/borutoapp/util/Constant.kt
@@ -2,6 +2,8 @@ package com.ahr.borutoapp.util
 
 object Constant {
 
+    const val BASE_URL = "http://10.0.2.2:8080"
+
     const val PREFERENCES_NAME = "boruto_preferences"
     const val PREFERENCES_ON_BOARDING_KEY = "on_boarding"
 
